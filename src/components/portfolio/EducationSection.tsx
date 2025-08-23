ed259e24468555647fc5d4d98ef58c3b505677c2
shivasai-muthyam <muthyamshivasai2004@gmail.com>
2025-08-23 18:32:58 +0530

    Update EducationSection.tsx
    updated the new certificate

--- src/components/portfolio/EducationSection.tsx
+++ src/components/portfolio/EducationSection.tsx
@@ -57,8 +57,7 @@ const EducationSection = () => {
       issuer: "Smart Interviews",
       date: "2025",
       type: "Completion Certificate",
-      description: "Completed a structured certification program in Data Structures and Algorithms, solving 150+ problems and gaining strong
-problem-solving skills coding interviews..",
+      description: "Completed a structured certification program in Data Structures and Algorithms, solving 150+ problems and gaining strong problem-solving skills coding interviews..",
       link: "https://smartinterviews.in/certificate/6791f243"
     },
   ];
